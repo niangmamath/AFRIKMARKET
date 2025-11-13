b95281d4f28c2ec0070845ebbf5a16e6530aaad7
niangmamath <niangmamathyacine@email.com>
2025-11-13 16:44:03 +0100

    Change port

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.use('/', adminRoutes);
 app.use('/', blogRoutes);
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
 });
